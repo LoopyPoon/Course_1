888aafc83b6c3022bf2ec119f6db4fa4f19b19b4
Poon <ivanholin479@gmail.com>
2023-03-25 03:37:42 +0500

    Lab 10

---
+++ src/Lab_10/Example1/XmlParser.java
@@ -0,0 +1,42 @@
+package Lab_10.Example1;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+
+public class XmlParser {
+    public static void main(String[] args) {
+        try {
+            File inputFile = new File("src/Lab_10/Example1/example.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+            System.out.println("Корневой элемент: " + doc.getDocumentElement().getNodeName());
+            NodeList nodeList = doc.getElementsByTagName("book");
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                System.out.println("\nТекущий элемент: " + node.getNodeName());
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) node;
+                    System.out.println("Название книги: " + element.getElementsByTagName("title").item(0)
+                            .getTextContent());
+                    System.out.println("Автор: " + element.getElementsByTagName("author").item(0)
+                            .getTextContent());
+                    System.out.println("Год издвания: " + element.getElementsByTagName("year").item(0)
+                            .getTextContent());
+                }
+            }
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/Lab_10/Example4/ReadExcelFileExample.java
@@ -0,0 +1,36 @@
+package Lab_10.Example4;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ReadExcelFileExample {
+    public static void main(String[] args) throws IOException {
+        // Открываем файл Excel для чтения
+        String filePath = "src/Lab_10/Example4/example4.xlsx";
+        FileInputStream inputStream = new FileInputStream(filePath);
+
+        // Создаем экземпляр книги Excel из файла
+        XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
+
+        // Получаем лист из книги по его имени
+        XSSFSheet sheet = workbook.getSheet("Товары");
+
+        // Перебираем строки и ячейки на листа
+        for (Row row : sheet) {
+            for (Cell cell : row) {
+                // Выводим значение ячейки на экран
+                System.out.print(cell.toString() + "\t");
+            }
+            System.out.println();
+        }
+
+        // Закрываем файл и освобождаем ресурсы
+        workbook.close();
+        inputStream.close();
+    }
+}
