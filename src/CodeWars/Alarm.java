2b3b9301343c76826245594606008a3dbc1b606c
Poon <ivanholin479@gmail.com>
2023-05-28 14:21:32 +0500

    cw

--- src/CodeWars/Alarm.java
+++ src/CodeWars/Alarm.java
@@ -2,12 +2,20 @@ package CodeWars;
 
 public class Alarm {
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println(sayAlarm(true, false));
         System.out.println(sayAlarm(true, true));
         System.out.println(sayAlarm(false, false));
         System.out.println(sayAlarm(false, true));
     }
 
+=======
+        System.out.println(sayAlarm(true,false));
+        System.out.println(sayAlarm(true,true));
+        System.out.println(sayAlarm(false,false));
+        System.out.println(sayAlarm(false,true));
+    }
+>>>>>>> 1f2e59d (cw)
     public static boolean sayAlarm(boolean employed, boolean vacation) {
         return employed & !vacation;
     }
